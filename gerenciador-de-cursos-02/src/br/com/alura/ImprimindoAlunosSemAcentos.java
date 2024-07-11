b5b5d446105403fa4fa0e83f224b4f13741146d5
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-11 15:04:32 -0300

    Elementos duplicados

--- gerenciador-de-cursos-02/src/br/com/alura/ImprimindoAlunosSemAcentos.java
+++ gerenciador-de-cursos-02/src/br/com/alura/ImprimindoAlunosSemAcentos.java
@@ -8,14 +8,23 @@ public class ImprimindoAlunosSemAcentos {
 	public static void main(String[] args) {
 
 		Set<String> alunos = new HashSet<>();
-		alunos.add("Paulo");
-		alunos.add("Alberto");
-		alunos.add("Pedro");
-		alunos.add("Nico");
+
+		// 1) adicione alguns alunos
 		
-		for (String aluno : alunos) {
-			System.out.println(aluno);
-		}
+		alunos.add("Otavio");
+		alunos.add("Henrique");
+		alunos.add("Walter");
+		// 2) imprima o tamanho da colecao
+		System.out.println(alunos.size());
+
+		// 3) tente adicionar um aluno que existe
+		
+		boolean adicionou = alunos.add("Otavio");
+		System.out.println("Otavio foi adicionado ao Set? " + adicionou);
+		// 4) imprima novamente o tamanho da colecao
+		
+		System.out.println(alunos.size());
+
 	}
 
 }
