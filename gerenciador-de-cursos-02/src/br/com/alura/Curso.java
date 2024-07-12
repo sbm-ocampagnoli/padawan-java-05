a2e81bb8003cf2934cce6dbd387575cd42f51752
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-12 09:49:35 -0300

    Encapsulando o Set

--- gerenciador-de-cursos-02/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos-02/src/br/com/alura/Curso.java
@@ -1,16 +1,19 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
-		this.nome = nome;
+		this.nome = nome;	
 		this.instrutor = instrutor;
 	}
 
@@ -42,6 +45,14 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "[Curso:" + this.getNome() + ",tempo total:" + this.getTempoTotal() + ", aulas: [" + this.aulas + "] ]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 
 }
